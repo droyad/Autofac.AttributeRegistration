ecb3a5b76c5102931191cf98cf2c061d49c373cc
Robert Wagner <robert.wagner@readify.net>
2013-10-29 21:09:58 +1000

    Added test for Winstore apps

---
+++ WindowsStoreTest/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Autofac;
+
+namespace WindowsStoreTest
+{
+    [TestFixture]
+    public class Test
+    {
+
+        public static void Main()
+        {
+            
+        }
+
+        [Test]
+        public void NugetPackageIsInstalled()
+        {
+            Assert.AreEqual(GetVersion<Test>(), GetVersion<SingleInstance>());
+        }
+
+        private string GetVersion<T>()
+        {
+            return Regex.Match(typeof(T).AssemblyQualifiedName, @"Version=([0-9\.]+)").Value;
+        }
+    }
+}
