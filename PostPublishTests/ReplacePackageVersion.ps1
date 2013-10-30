1bec36aed91b760226a69d44c948ce5fa05bcab2
Robert Wagner <robert.wagner@readify.net>
2013-10-30 20:55:00 +1000

    Added ReplacePackageVersion Script

---
+++ PostPublishTests/ReplacePackageVersion.ps1
@@ -0,0 +1,15 @@
+param([parameter(Mandatory=$true)] [string]$version)
+
+$configs = (ls -r packages.config)
+foreach($config in $configs)
+{
+	 [xml] $xml = (Get-Content $config)
+	 foreach($package in $xml.packages.package)
+	 {
+		if($package.id -eq 'Autofac.AttributeRegistration')
+		{
+			$package.version = '$version';
+		}
+	 }
+	 $xml.Save($config);
+}
